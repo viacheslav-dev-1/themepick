380240df39676fe08b4650ed840ba9e02b142a54
viacheslav <bviacheslav.12@gmail.com>
2023-05-11 22:08:53 +0300

    Added project skeleton

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import pkg from "./package.json";
+import terser from "@rollup/plugin-terser";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+
+export default [
+    {
+        input: "src/index.js",
+        output: [
+            { name: "themepick", file: pkg.browser, format: "umd" },
+            { file: pkg.module, format: "es" }
+        ],
+        plugins: [
+            nodeResolve({ browser: true }),
+            commonjs(),
+            babel({
+                exclude: ["node_modules/**"]
+            }),
+            terser()
+        ],
+    },
+    {
+        input: "src/index.js",
+        output: [
+            { file: pkg.main, format: "cjs" }
+        ],
+        plugins: [
+            nodeResolve()
+        ]
+    },
+];
